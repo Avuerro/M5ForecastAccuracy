8ea3d3187951b2cbc86cfec41faf2a6a35c7ba91
Avuerro <>
2020-06-14 21:55:26 +0200

    added wand callback to model evaluate call

--- train/cross_validate.py
+++ train/cross_validate.py
@@ -14,8 +14,7 @@ def cross_validate(model, X,y, batch_size, epochs, number_of_folds,callbacks):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
     n_model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=callbacks, verbose=1)
-    scores = n_model.evaluate(X_test, y_test, verbose=1)
-    
+    scores = n_model.evaluate(X_test, y_test, callbacks=[callbacks[1]], verbose=1) # we only want the wandb callback..
     print("%s: %.5f%%" % (n_model.metrics_names[0], scores[0]))
     cv_scores.append(scores[0])
   return cv_scores, np.mean(cv_scores), np.std(cv_scores)
